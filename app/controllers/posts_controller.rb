b450cdaf8929b936d7d833c3f045b864284bac3b
Sajidur Rahman <sajibsrs@gmail.com>
2020-03-22 15:57:32 +0600

    bootstrap added to views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,9 +24,16 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to(@post, notice: 'Post updated successfully')
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
