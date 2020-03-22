5e8b0ca92ddc5e8bcc7509ccd075b0790035acc9
Sajidur Rahman <sajibsrs@gmail.com>
2020-03-22 23:55:53 +0600

    style and layout change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :user_full_name
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[fname lname])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[fname lname])
-  end
-
-  def user_full_name
-    return unless current_user
-
-    @user_full_name = current_user.fname + ' ' + current_user.lname
   end
 end
