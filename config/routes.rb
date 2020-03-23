a743742cf192a270fffaaf3941e45f8cacd6da88
Sajidur Rahman <sajibsrs@gmail.com>
2020-03-23 16:18:01 +0600

    removed generated code, set index to post index

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/show'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/destroy'
-  root to: 'users#index'
+  root to: 'posts#index'
   devise_for :users
   resources :users
   resources :posts
