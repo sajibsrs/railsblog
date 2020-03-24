7f72d46f6c2dbc71af0ba2b2c1ea6aaf275414c0
Sajidur Rahman <sajibsrs@gmail.com>
2020-03-24 15:39:15 +0600

    destroy method added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -37,6 +37,9 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: 'Post deleted successfully'
   end
 
   def post_params
