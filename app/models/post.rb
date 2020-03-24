2857a9611ca0b78bb689acee29fdaf5249280b1a
Sajidur Rahman <sajibsrs@gmail.com>
2020-03-25 03:33:55 +0600

    removed post content, added rich text

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
+
   validates :user_id, presence: true
   validates :title, presence: true
   validates :content, presence: true
+
+  has_rich_text :content
 end
